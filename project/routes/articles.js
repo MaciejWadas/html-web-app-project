df2847f949ddd1588a275d28b4f0041729c5a7a9
MaciejWadas <wadas.maciej@gmail.com>
2026-02-13 22:29:11 +0100

    Implemented tags, and article filtering.

--- project/routes/articles.js
+++ project/routes/articles.js
@@ -4,12 +4,12 @@ var express = require('express');
 var router = express.Router();
 var mysql = require('mysql2');
 
-const {DB_HOST, DB_USER, DB_PASSWORD, DB_DATABASE} = process.env;
+const {DB_HOST, DB_USER, DB_PASSWORD, DB_NAME} = process.env;
 const db = mysql.createConnection({
     host: DB_HOST,
     user: DB_USER,
     password: DB_PASSWORD,
-    database: DB_DATABASE
+    database: DB_NAME
 })
 
 const getAllArticles =(req, res, next) => {
@@ -17,7 +17,12 @@ const getAllArticles =(req, res, next) => {
     if (req.query.limit) {
         limit = parseInt(req.query.limit)+4;
     }
-    var query = 'SELECT * FROM posts LIMIT '+limit+";";
+    let query = 'SELECT * FROM posts LIMIT '+limit+";";
+    if (req.query.tag) {
+        let tag = parseInt(req.query.tag);
+        query = `SELECT * FROM posts p RIGHT JOIN article_tags a ON p.postID = a.postID WHERE a.tagID = ${tag} LIMIT ${limit}`;
+        req.tag = tag;
+    }
     db.query(query, (err, result) => {
         if (err) {
             console.log("Error fetching articles from the database: "+err);
@@ -25,6 +30,34 @@ const getAllArticles =(req, res, next) => {
         }
         req.articles = result;
         req.limit = result.length;
+        next();
+    })
+}
+
+const getAllTags =(req, res, next) => {
+    var query = 'SELECT * FROM tags;';
+    db.query(query, (err, result) => {
+        if (err) {
+            console.log("Error fetching tags from the database: "+err);
+            return res.status(500).send("Database error");
+
+        }
+        req.tags = result;
+        if (req.tag) {
+            req.tag = req.tags[req.tag-1];
+        }
+        next();
+    })
+}
+
+const getTagsForArticle = (req, res, next) => {
+    var query = `SELECT * FROM article_tags a LEFT JOIN tags t ON a.tagID = t.TagID WHERE a.postID = ${req.article[0].PostID}`;
+    db.query(query, (err, result) => {
+        if (err) {
+            console.log("Error fetching tags from the database: "+err);
+            return res.status(500).send("Database error");
+        }
+        req.tags = result;
         next();
     })
 }
@@ -50,19 +83,21 @@ db.connect(err => {
 });
 
 /* GET home page. */
-router.get('/',getAllArticles, (req, res) => {
-console.log(req.articles,req.articles.length);
+router.get('/',getAllArticles,getAllTags, (req, res) => {
     res.render('articles', {
         title: 'The Witcher - Wiki',
         articles: req.articles,
         limit: req.limit,
+        tags: req.tags,
+        active_tag: req.tag
     });
 });
 
-router.get('/:id', getArticle, (req, res) =>{
+router.get('/:id', getArticle, getTagsForArticle, (req, res) =>{
     res.render('article', {
         title: 'The Witcher - Wiki',
         article: req.article[0] || [],
+        tags: req.tags
     });
 })
 module.exports = router;
